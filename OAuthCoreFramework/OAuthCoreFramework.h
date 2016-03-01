f04d4a130c8f8cd4d77b0519695fd59facc23fd5
Torsten Louland <torsten.louland@satisfyingstructures.com>
2016-03-01 20:31:14 +0100

    Carthage support: add Xcode project to build OAuthCore as framework (iOS & OSX)

---
+++ OAuthCoreFramework/OAuthCoreFramework.h
@@ -0,0 +1,19 @@
+//
+//  OAuthCoreFramework.h
+//  OAuthCore
+//
+//  Created by Torsten Louland on 01/03/2016.
+//  Copyright © 2016 Satisfying Structures BVBA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OAuthCore.
+FOUNDATION_EXPORT double OAuthCoreVersionNumber;
+
+//! Project version string for OAuthCore.
+FOUNDATION_EXPORT const unsigned char OAuthCoreVersionString[];
+
+// Public header imports
+#import <OAuthCore/OAuthCore.h>
+
